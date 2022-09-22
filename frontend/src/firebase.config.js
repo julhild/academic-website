77bbc2dcfef8c934f9c5c2d639611c14e16bb118
Julia Hildmann <julia_hildmann@web.de>
2022-09-22 14:21:02 -0400

    delete server

---
+++ frontend/src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpOYBafD6U_W3eqm61vhdhAaUBo8S0hT8",
+  authDomain: "cat-sciense-website.firebaseapp.com",
+  projectId: "cat-sciense-website",
+  storageBucket: "cat-sciense-website.appspot.com",
+  messagingSenderId: "101418695136",
+  appId: "1:101418695136:web:8e84f245548c92c6535385",
+  measurementId: "G-2X094PNDBE",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
